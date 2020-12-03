719d5cbe6e62433535ce62f2a171479098d4a40a
Taylor Page <taylorlpage@yahoo.com>
2020-12-03 11:23:17 -0500

    Test CodeSandbox Checkin Testing git checkin

--- Elementary/17_IsEven.ts
+++ Elementary/17_IsEven.ts
@@ -1,11 +1,11 @@
-import * as assert from "assert"
+import * as assert from "assert";
 
 function isEven(num: number): boolean {
-    return !(num % 2)
+  return !(num % 2);
 }
 
-const isEven2 = (num: number): boolean => !(num % 2)
+//const isEven = (num: number): boolean => !(num % 2)
 
-assert.deepEqual(isEven(2), true)
-assert.deepEqual(isEven(5), false)
-assert.deepEqual(isEven(0), true)
+assert.deepEqual(isEven(2), true);
+assert.deepEqual(isEven(5), false);
+assert.deepEqual(isEven(0), true);
