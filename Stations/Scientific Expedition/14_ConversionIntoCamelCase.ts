9eb2be302a7e2a1b4959426ead7d7b32142a1f17
Taylor Page <taylor.page@dditechnology.com>
2021-09-23 09:01:32 -0400

    Incremental Check In

--- Stations/Scientific Expedition/14_ConversionIntoCamelCase.ts
+++ Stations/Scientific Expedition/14_ConversionIntoCamelCase.ts
@@ -1,15 +1,27 @@
-import assert from "assert"
+import * as assert from "assert"
 
-function toCamelCase(name: string): string {
+function toCamelCase(name: string): any {
     // 1: Replace Regex to get groupings
-    // 2: Remove "_" from grouping and uppercase
-    return name.replace(/([a-z]_)([a-z])/g, "$1$2".toUpperCase()) //v => v.replace(/[_]/,"").toUpperCase())
+    // 2: Remove "_" from grouping and uppercasereturn `${name[0].toUpperCase()}${name.slice(1).replace(/\w[_]\w/ig,(w) => w[1].toUpperCase())}`
+    //return name.replace(/([a-z]+)|([\P{Pc}][a-z]+)/gimu, w => `${w[0].toUpperCase()}${w.slice(1)}`).replace(/_/gmi,"")
+    return name.split("_").map(w => `${w[0].toUpperCase()}${w.slice(1)}`).join("")
+    //return name.replace(/(?<word>[a-z]+)/gi, w => w) //(w => w[0].toUpperCase()+w.slice(1)).join("")
+    return `${name[0].toUpperCase()}${name.slice(1)}`
+            .replace(/([\P{Pc}]+)([a-z]+)/gimu, w => `${w[0].toUpperCase()}${w.slice(1)}`)
+    return name.replace(/(?<word>\w+)(?<under>[\P{Pc}]?)/gimu, "$<word>")
+    return name.match(/([a-z]+)|(^[\p{Pc}][a-z]+)/gimu)
+    return name.replace(/(!([a-z]+)(?=[\P{Pc}])|(?<=[\P{Pc}])([a-z]+))/gimu, "")
+    return name.match(/([\P{Pc}]+)/gimu)
+
+    //return name.replace(/(_)([a-z])/gmi, "$2".toUpperCase()) //"$1$2".toUpperCase()) //v => v.replace(/[_]/,"").toUpperCase())
 }
 
 console.log(toCamelCase("my_function_name"))
+console.log(toCamelCase("i_phone"))
 
-/*
 assert.deepStrictEqual(toCamelCase("my_function_name"), "MyFunctionName")
 assert.deepStrictEqual(toCamelCase("i_phone"), "IPhone")
-*/
+
 //(?:^\w|[A-Z]|[_]\w)
+
+//(([\P{Pc}]?)([a-z]+))+
--- Tests/ElementaryTests/01_multTwo.test.ts
+++ Tests/ElementaryTests/01_multTwo.test.ts
@@ -1,5 +1,5 @@
-import * as assert from "assert"
-import multTwo from "../../Elementary/01_MultiplyIntro";
+import * as assert from "assert";
+import multTwo from "../../Stations/Elementary/01_MultiplyIntro";
 
 describe("Practice Examples", () => {
     it("3 * 2 = 6", () => {
